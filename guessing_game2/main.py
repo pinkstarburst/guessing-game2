bd28fc9a5fce7108877b65138db43614a41a7fd1
Jamie Raymond <eversobrainless@gmail.com>
2026-06-22 09:25:04 -0400

    Added three attempt guessing system

--- guessing_game2/main.py
+++ guessing_game2/main.py
@@ -1,18 +1,20 @@
 import random
 
 secret_number = random.randint(1, 10)
-guess = int(input("Guess a number from 1-10"))
 
-if guess == secret_number:
-    print("You Win!")
-else:
-    print(f"You Lose! The number was {secret_number}")
+print("Welcome to the Guessing Game!")
+print("You have 3 attempts.")
+
+for attempt in range(3):
+    guess = int(input("Guess a number from 1-10: "))
+
+    if guess == secret_number:
+        print("🎉 You win!")
+        break
+    else:
+        print("Wrong!")
+
+if guess != secret_number:
+    print(f"❌ You lose! The number was {secret_number}")
 
 print("Thanks for playing!")
-
-
-
-
-print("this is guessing game 2.0")
-
-
