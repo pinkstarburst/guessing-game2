425eeb3f72b22db0d01dce84bfe1fe61676b4dd6
Jamie Raymond <eversobrainless@gmail.com>
2026-06-22 09:19:03 -0400

    Fixed number display and added welcome message

--- guessing_game2/main.py
+++ guessing_game2/main.py
@@ -1,1 +1,18 @@
+import random
+
+secret_number = random.randint(1, 10)
+guess = int(input("Guess a number from 1-10"))
+
+if guess == secret_number:
+    print("You Win!")
+else:
+    print(f"You Lose! The number was {secret_number}")
+
+print("Thanks for playing!")
+
+
+
+
 print("this is guessing game 2.0")
+
+
